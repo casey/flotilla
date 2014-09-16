cc22d1ec545b58ad6c6aabdb9a684622db8a825f
Casey Rodarmor <casey@rodarmor.com>
2014-09-16 22:08:57 +0900

    be liberal with CORS

--- respond.go
+++ respond.go
@@ -20,13 +20,12 @@ func Respond(w http.ResponseWriter, r *http.Request) {
 
   c.Infof("handler: why: %v", response.why)
 
-  if r.Method == "OPTIONS" && response.status == 200 {
-    w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, TRACE, CONNECT")
-  }
-
   w.Header().Set("License", "Anyone may do anything with this.")
   w.Header().Set("Warranty", `"AS IS" WITH NO WARRANTY OF ANY KIND EXPRESS OR IMPLIED.`)
   w.Header().Set("Access-Control-Allow-Origin", "*")
+  w.Header().Set("Access-Control-Allow-Headers", "*")
+  w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, TRACE, CONNECT")
+  w.Header().Set("Access-Control-Max-Age", "1728000")
   w.Header().Set("Content-Type", response.mimetype)
   w.WriteHeader(response.status.number())
   w.Write([]byte(response.body))
