75a05936660989dab6defb9f5cb0408abbde2a1c
Casey Rodarmor <casey@rodarmor.com>
2014-09-16 22:02:18 +0900

    add cors to OPTIONS

---
+++ handle.go
@@ -0,0 +1,35 @@
+package flotilla
+
+import "net/http"
+
+func Handle(pattern string) *Handler {
+  this := new(Handler)
+  this.methods = make(map[string]func(*http.Request))
+  
+  http.HandleFunc(pattern, func (w http.ResponseWriter, r *http.Request) {
+    defer Respond(w, r)
+    f, ok := this.methods[r.Method]
+    Ensure(ok, http.StatusMethodNotAllowed)
+    f(r)
+  })
+
+  return this
+}
+
+type Handler struct {
+  methods map[string]func(r *http.Request)
+};
+
+func (this *Handler) On(method string, f func(*http.Request)) *Handler {
+  _, ok := this.methods[method]
+  if ok {
+    panic("Handler: duplicate method handler: " + method)
+  }
+  this.methods[method] = f
+  return this
+}
+
+func (this *Handler) Get    (f func(*http.Request)) *Handler { return this.On("GET",     f) }
+func (this *Handler) Put    (f func(*http.Request)) *Handler { return this.On("PUT",     f) }
+func (this *Handler) Options(f func(*http.Request)) *Handler { return this.On("OPTIONS", f) }
+
--- respond.go
+++ respond.go
@@ -20,6 +20,10 @@ func Respond(w http.ResponseWriter, r *http.Request) {
 
   c.Infof("handler: why: %v", response.why)
 
+  if r.Method == "OPTIONS" && response.status == 200 {
+    w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, TRACE, CONNECT")
+  }
+
   w.Header().Set("License", "Anyone may do anything with this.")
   w.Header().Set("Warranty", `"AS IS" WITH NO WARRANTY OF ANY KIND EXPRESS OR IMPLIED.`)
   w.Header().Set("Access-Control-Allow-Origin", "*")
